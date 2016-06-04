5d305e6c70ef3cb15821321b913d2c159e61ced4
False_Chicken <jmdevsupport@gmail.com>
2016-06-04 16:28:41 -0400

    - Added command_exec. DANGEROUS: Allows users to execute arbitrary code. Use with caution.

---
+++ plugins/command_exec.lua
@@ -0,0 +1,45 @@
+--[[
+Command_Exec
+!WARNING WARNING : DANGEROUS DANGEROUS!
+Allows users to run arbitrary Lua code. You obviously do not want users running random code on your machine. Use with caution.
+Licensed Under the GPLv2
+--]]
+
+local perms = require("riddim/ai_utils/permissions");
+local jid_tool = require("riddim/ai_utils/jid_tool");
+
+local BOT;
+
+local CheckPermissions = function(_command)
+	if _command.stanza.attr.type == "groupchat" then
+		if perms.HasPermission(_command.sender["jid"], "command_exec", BOT.config.permissions) == false then
+			return false;
+		end
+		return true;
+	else
+		if perms.HasPermission(jid_tool.StripResourceFromJID(_command.sender["jid"]), "command_exec", BOT.config.permissions) == false then
+			return false;
+		end
+		return true;
+	end
+end
+
+function riddim.plugins.command_exec(_bot)
+	_bot:hook("commands/exec", ParseExecCommand);
+	BOT = _bot;
+end
+
+function ParseExecCommand(_command)
+
+	if CheckPermissions(_command) then
+		local f = loadstring(_command.param);
+		local r, e = pcall(f);
+		if r then _command:reply(tostring(e));
+		else _command:reply("Error: "..tostring(e)); end
+		return;
+	else
+		_command:reply("You are not authorized to run this command.");
+		return;
+	end
+
+end
