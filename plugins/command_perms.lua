bd6d0714e6d66cdc845f52b696abcd17005f6d59
False_Chicken <jmdevsupport@gmail.com>
2016-06-05 01:09:40 -0400

    - Added command_weather. Allows users to set their zip code and request weather and forecast information. - Added command_perms. Allows users list their current permissions.

---
+++ plugins/command_perms.lua
@@ -0,0 +1,52 @@
+--[[
+Command_Perms
+Allows users to check their permissions. (Requires ai_utils from ai_loader)
+Licensed Under the GPLv2
+--]]
+
+local permissions = require("riddim/ai_utils/permissions");
+local jid_tool = require("riddim/ai_utils/jid_tool");
+
+
+local BOT;
+
+local CheckPermissions = function(_command)
+	if _command.stanza.attr.type == "groupchat" then
+		if permissions.HasPermission(_command.sender["jid"], "command_perms", BOT.config.permissions) == false then
+			return false;
+		end
+		return true;
+	else
+		if permissions.HasPermission(jid_tool.StripResourceFromJID(_command.sender["jid"]), "command_perms", BOT.config.permissions) == false then
+			return false;
+		end
+		return true;
+	end
+end
+
+function riddim.plugins.command_perms(_bot)
+	_bot:hook("commands/perms", ParsePermsCommand);
+	BOT = _bot;
+end
+
+function ParsePermsCommand(_command)
+
+	if CheckPermissions(_command) then
+		local permsList = "\n-- Your Permissions --";
+		if _command.stanza.attr.type == "groupchat" then
+			for key, permission in pairs(BOT.config.permissions[_command.sender["jid"]]) do
+				permsList = permsList.."\n - "..permission;
+			end
+			permsList = permsList.."\n----------------";
+			return permsList;
+		else
+			for key, permission in pairs(BOT.config.permissions[jid_tool.StripResourceFromJID(_command.sender["jid"])]) do
+				permsList = permsList.."\n - "..permission;
+			end
+			permsList = permsList.."\n----------------";
+			return permsList;
+		end
+	else
+		return "You are not authorized to run this command.";
+	end
+end
---
+++ plugins/command_weather.lua
@@ -0,0 +1,71 @@
+--[[
+Licensed Under the GPLv2
+--]]
+
+local datamanager = require("util.datamanager");
+local jidTool = require("riddim/ai_utils/jid_tool");
+local stanzaUtils = require("riddim/ai_utils/stanzautils");
+local tableUtils = require("riddim/ai_utils/tableutils");
+
+local BOT;
+
+function riddim.plugins.command_weather(_bot)
+	_bot:hook("commands/weather", ParseWeatherCommand);
+	_bot:hook("commands/forecast", ParseForecastCommand);
+	_bot:hook("commands/weatherset", ParseWeatherSetCommand);
+	BOT = _bot;
+end
+
+function ParseWeatherCommand(_command)
+
+	if stanzaUtils.IsGroupChat(_command) then return "This command cannot be used in group chat."; end
+	
+	local jid, host, resource = jidTool.SeperateFullJID(_command.sender["jid"]);
+	
+	local zipStorage = datamanager.load(jid, host, "weatherZip");
+	
+	if zipStorage == nil then return "I don't know your zip code! Set it with: @weatherset <Zip Code>."; end
+	
+	local handle = io.popen("weather-util "..zipStorage[1]);
+	local result = "\n"..handle:read("*a");
+	handle:close();
+	
+	return result;
+
+end
+
+function ParseForecastCommand(_command)
+
+	if stanzaUtils.IsGroupChat(_command) then return "This command cannot be used in group chat."; end
+	
+	local jid, host, resource = jidTool.SeperateFullJID(_command.sender["jid"]);
+	
+	local zipStorage = datamanager.load(jid, host, "weatherZip");
+	
+	if zipStorage == nil then return "I don't know your zip code! Set it with: @weatherset <Zip Code>."; end
+	
+	local handle = io.popen("weather-util -f "..zipStorage[1]);
+	local result = "\n"..handle:read("*a");
+	handle:close();
+	
+	return result;
+	
+end
+
+function ParseWeatherSetCommand(_command)
+
+	if stanzaUtils.IsGroupChat(_command) then return "This command cannot be used in group chat."; end
+  
+	if _command.param == nil then return "Missing zip code!"; end
+	
+	local jid, host, resource = jidTool.SeperateFullJID(_command.sender["jid"]);
+	
+	local zipStorage = {};
+	
+	zipStorage[1] = _command.param;
+		
+	datamanager.store(jid, host, "weatherZip", zipStorage);
+			
+	return "Saved weather zip.";
+
+end
